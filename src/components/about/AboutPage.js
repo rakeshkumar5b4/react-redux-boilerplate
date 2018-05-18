6b56a0130314cdb647cc2a743ae566bd48161a2e
Rakesh Kumar Singitham <rakhi.b4@gmail.com>
2018-05-18 13:35:59 +0530

    added home page , about page , header along with the styles which are imported in es6 format

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This application uses React, Redux, React Router and many other helpful
+                    libraries!!</p>
+            </div>
+        );
+    }
+
+}
+
+export default AboutPage;
